550ff672997f049d6b646d635f2436a35365506b
linsalrob <raedwards@gmail.com>
2013-07-03 14:40:51 -0700

    updating analysis and rf code

--- CoralImageClassification/v.2.0/src/analyzeDZImages.py
+++ CoralImageClassification/v.2.0/src/analyzeDZImages.py
@@ -24,6 +24,8 @@ parser.add_argument('-d', '--directory', help='The directory of images to analys
 parser.add_argument('-f', '--file', help='File of images and their classifications')
 parser.add_argument('-t', '--tab', help='Classification is tab separated text (default: Zawada format)')
 parser.add_argument('-a', '--all', help='Process all files (otherwise only those in the classification file are processed). If no file is given, this is assumed', action='store_true')
+parser.add_argument('-o', '--output', help='The output file. If this file exists, images in the file will be skipped and the file will be appended', required=True)
+parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
 args = parser.parse_args()
 
 if not args.file:
@@ -43,8 +45,8 @@ lap = Laplacian()
 # Edges has CAnny
 edge = Edges()
 
-# we initialize contours later so we can set the image
 
+# initiate and run the classification if needed
 classification = {}
 if args.file:
     classifier = Classification.Parsers(args.file)
@@ -54,63 +56,97 @@ if args.file:
         classification = classifier.zawada()
 
 
-print ('\t'.join(map(str, ["Image File", "Classification", 
-       "grayMin", "grayMax", "grayMedian", "grayMean", 
-       "gnMin", "gnMax", "gnMedian", "gnMean", 
-       "blueMin", "blueMax", "blueMedian", "blueMean", 
-       "greenMin", "greenMax", "greenMedian", "greenMean", 
-       "redMin", "redMax", "redMedian", "redMean", 
-       "FFT", "nFFT",]))), "\t",
-for i in range(15):
-    print "Lapl", (2*i+1), "\t",
+# set up the output file
 
-for i in range(25):
-    print "Can1.", (10*i), "\t",
+fout=None;
+seen={}
+if (os.path.exists(args.output)):
+    # if the output exists we need to read it and collect all the files we have seen
+    fout=open(args.output, 'r')
+    for line in fout:
+        pieces = line.split('\t')
+        seen[pieces[0]]=1
+    fout.close()
+    fout=open(args.output, 'a')
+else:
+    # if we have created a new output file we need to print the header. 
+    # If we are appending to an existing output file we do not need to create the header
+    fout=open(args.output, 'w')
+    fout.write( ('\t'.join(map(str, ["Image File", "Classification", 
+           "grayMin", "grayMax", "grayMedian", "grayMean", 
+           "gnMin", "gnMax", "gnMedian", "gnMean", 
+           "blueMin", "blueMax", "blueMedian", "blueMean", 
+           "greenMin", "greenMax", "greenMedian", "greenMean", 
+           "redMin", "redMax", "redMedian", "redMean", 
+           "FFT", "nFFT",]))) +"\t")
+    for i in range(15):
+        fout.write( "Lapl" + str(2*i+1) + "\t")
 
-for i in range(5):
-    t=50*i
-    print "Contours"+str(t), "\tClosedCont"+str(t), "\tOpenCont"+str(t), "\tContArea"+str(t), "\tLargestCont"+str(t), "\tPerimeter"+str(t), "\t",
-    print "MaxLL"+str(t), "\tMeanLL"+str(t), "\tMedianLL"+str(t), "\tModeLL"+str(t), "\t",
+    for i in range(25):
+        fout.write( "Can1." + str(10*i) + "\t")
 
+    for i in range(5):
+        t=50*i
+        fout.write( "Contours" + str(t) + "\tClosedCont" + str(t) + "\tOpenCont" + str(t) + "\tContArea" + str(t) + "\tLargestCont" + str(t) + "\tPerimeter" + str(t) + "\t")
+        fout.write( "MaxLL" + str(t) + "\tMeanLL" + str(t) + "\tMedianLL" + str(t) + "\tModeLL" + str(t) + "\t")
 
-print
+    fout.write("\n")
+
 
 images = os.listdir(args.directory)
 for imgfile in images:
+
+    if imgfile in seen:
+        continue
+
     if os.path.isdir(os.path.join(args.directory, imgfile)):
         temp = os.listdir(os.path.join(args.directory, imgfile))
         for f in temp:
             images.append(os.path.join(imgfile, f))
         continue
+
     if not args.all and imgfile not in classification:
         continue
-    print imgfile, "\t",
+
+    # silently skip the bin files that have the gps data
+    if imgfile.endswith('bin'):
+        continue
+    # alert to other files that were skipped
+    if not (imgfile.endswith('png') | imgfile.endswith('jpg')):
+        sys.stderr.write("Skipped file: " + imgfile + "\n")
+        continue
+
+    if args.verbose:
+        sys.stderr.write("Parsing " + imgfile + "\n")
+
+    fout.write( imgfile + "\t" )
     if imgfile in classification:
-        print classification[imgfile], "\t",
+        fout.write( classification[imgfile] + "\t")
     else:
-        print "\t",
+        fout.write( "\t" )
+
 
     img = ImageIO.cv2read(os.path.join(args.directory, imgfile))
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    print ('\t'.join(map(str, [stats.min(gray), stats.max(gray), stats.median(gray), stats.mean(gray)]))), "\t",
+    fout.write( ('\t'.join(map(str, [stats.min(gray), stats.max(gray), stats.median(gray), stats.mean(gray)]))) + "\t" )
 
     ngray = Normalization.equalizeHistograms(gray)
     # apply a gaussian blur to remove edge effects
     ngray = cv2.GaussianBlur(ngray, (3,3), 0)
-    print ('\t'.join(map(str, [stats.min(ngray), stats.max(ngray), stats.median(ngray), stats.mean(ngray)]))), "\t",
+    fout.write( ('\t'.join(map(str, [stats.min(ngray), stats.max(ngray), stats.median(ngray), stats.mean(ngray)]))) + "\t")
 
     for i in range(3):
         imp = img[:,:,i]
-        print ('\t'.join(map(str, [stats.min(imp), stats.max(imp), stats.median(imp), stats.mean(imp)]))), "\t",
-    print fft.energy(gray), "\t", fft.energy(ngray), "\t",
+        fout.write( ('\t'.join(map(str, [stats.min(imp), stats.max(imp), stats.median(imp), stats.mean(imp)]))) + "\t" )
+    fout.write( str(fft.energy(gray)) + "\t" + str(fft.energy(ngray)) + "\t")
 
     for i in range(15):
         k=2*i+1
-        print lap.sum(ngray, k), "\t",
+        fout.write( str(lap.sum(ngray, k)) + "\t")
 
     for i in range(25):
         t2 = 10*i
-        print edge.sumCanny(ngray, 1, t2), "\t",
+        fout.write( str(edge.sumCanny(ngray, 1, t2)) + "\t")
     #edge.sumCanny(gray)
 
     # Contour detection
@@ -119,16 +155,17 @@ for imgfile in images:
         threshold=50*i
         ctr.withCanny(1, threshold)
         if ctr.numberOfContours() == 0:
-            print "0\t0\t0\t0\t0\t0\t0\t0\t0\t0"
+            fout.write( "0\t0\t0\t0\t0\t0\t0\t0\t0\t0\t" )
         else:
             try:
-                print "\t".join(map(str, [ctr.numberOfContours(), ctr.numberOfClosedContours(),
+                fout.write( "\t".join(map(str, [ctr.numberOfContours(), ctr.numberOfClosedContours(),
                                       ctr.numberOfOpenContours(), ctr.totalContourArea(), cv2.contourArea(ctr.largestContourByArea()),
-                                      ctr.totalPerimeterLength()])), "\t",
+                                      ctr.totalPerimeterLength()])) + "\t")
                 ctr.linelengths()
-                print "\t".join(map(str, [ctr.maxLineLength(), ctr.meanLineLength(), ctr.medianLineLength(), ctr.modeLineLength()])), "\t",
-            except ValueError as e:
+                fout.write( "\t".join(map(str, [ctr.maxLineLength(), ctr.meanLineLength(), ctr.medianLineLength(), ctr.modeLineLength()])) + "\t")
+            except Exception as e:
                 sys.stderr.write("There was an error calculating the contours for " + imgfile +": " + e.message + "\n")
-                break
-    print
+                fout.write( "0\t0\t0\t0\t0\t0\t0\t0\t0\t0\t" )
+
+    fout.write("\n")
 
--- CoralImageClassification/v.2.0/src/randomForest.r
+++ CoralImageClassification/v.2.0/src/randomForest.r
@@ -4,8 +4,15 @@ print(paste("Parsing", infile, sep=" "))
 
 library('randomForest')
 data <- read.delim(infile)
+# trim off the last column which is all NA
+data <- data[,1:(ncol(data)-1)]
 data <- data[complete.cases(data),]
-rf=randomForest(data[,3:(ncol(data)-1)], data[,2], importance=TRUE, ntrees=50000)
+# remove any cases where we don't have classification data
+data <- subset(data, data[,2]!=0)
+data[,2]<-factor(data[,2])
+
+rf=randomForest(data[,3:ncol(data)], data[,2], importance=TRUE, ntrees=100001)
+rf
 outfile=paste("png/", infile, "_imp.png", sep="")
 png(outfile)
 par(mfrow=c(2,1))
@@ -13,4 +20,6 @@ par(pty="s")
 varImpPlot(rf, type=1, pch=19, col=1, cex=.5, main="")
 varImpPlot(rf, type=2, pch=19, col=1, cex=.5, main="")
 dev.off()
-rf
+p <- predict(rf, data, 'prob')
+outtext = paste(infile, ".probabilities.txt", sep="")
+write.table(p, file=outtext, sep='\t')
