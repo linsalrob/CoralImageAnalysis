dd6f680496965e3f025a74a30f6565861bcef1cb
Salvatore Tosti <saltosti@gmail.com>
2014-06-26 11:20:01 -0700

    Worked on ImageIO and Normalization unit tests
    Completed a basic Normalization unit test and started work on an
    ImageIO unit test.

--- CoralImageClassification/v.2.0/unittests/test_imageio_unittest.py
+++ CoralImageClassification/v.2.0/unittests/test_imageio_unittest.py
@@ -1,14 +1,35 @@
 #test_imageio_unittest.py
 #unittest class for ImageIO.py
 import unittest
+import sys
+import cv
+import cv2
+import Pillow
+sys.path.append('../Modules/')
+import ImageIO
 
 class TestImageIO(unittest.TestCase):
     
     def setUp(self):
+        self.imageFile = '../test/test.jpg'
+        
+    def test_cvread(self):
+        img = ImageIO.cvread(self.imageFile)
+        tmpImg = cv.LoadImage(self.imageFile)
+        
+        
+        #equalityBool 
+        #equalityBool = img == tmpImg
+        #self.assertTrue(equalityBool)
+    
+    def test_as_array(self):
         pass
-        #imageFile = './test/test.jpg'
-        #self.img = ImageIO.cv2read(imageFile)
-        #self.imageio = ImageIO()
+    
+    def test_cv2read(self):
+        pass
+    
+    def test_cv2grayscale(self):
+        pass
 
 if __name__=="__main__":
     unittest.main()
