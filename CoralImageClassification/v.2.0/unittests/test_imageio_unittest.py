f7e47fd2eb00e2a8021683ae452bbd8f736c7100
Salvatore Tosti <saltosti@gmail.com>
2014-06-27 09:59:27 -0700

    Finished test_imageio_unittest.py
    Completed initial unit tests on methods within ImageIO.py

--- CoralImageClassification/v.2.0/unittests/test_imageio_unittest.py
+++ CoralImageClassification/v.2.0/unittests/test_imageio_unittest.py
@@ -4,7 +4,9 @@ import unittest
 import sys
 import cv
 import cv2
-import Pillow
+import numpy
+
+#import Pillow
 sys.path.append('../Modules/')
 import ImageIO
 
@@ -16,20 +18,34 @@ class TestImageIO(unittest.TestCase):
     def test_cvread(self):
         img = ImageIO.cvread(self.imageFile)
         tmpImg = cv.LoadImage(self.imageFile)
+        cv.AbsDiff(img,tmpImg,img)
+  
+        mat = cv.GetMat(img)
+        arr = numpy.asarray(mat)
         
-        
-        #equalityBool 
-        #equalityBool = img == tmpImg
-        #self.assertTrue(equalityBool)
+        equalityBool = (arr.all() == 0)
+        self.assertTrue(equalityBool)
     
     def test_as_array(self):
-        pass
+        """This method is broken and will not run.
+        The as_array method attempts to use the argument 'source' which is undefined.
+        The notes state that cv2read should be used instead."""
+        #img = ImageIO.as_array(self.imageFile)
+        self.skipTest("Not implemented yet.")
     
     def test_cv2read(self):
-        pass
+        img = ImageIO.cv2read(self.imageFile)
+        tmpImg = cv2.imread(self.imageFile)
+        
+        equalityBool = (img == tmpImg).all()
+        self.assertTrue(equalityBool)
     
     def test_cv2grayscale(self):
-        pass
+        imgGray = ImageIO.cv2grayscale(self.imageFile)
+        tmpImgGray = cv2.imread(self.imageFile)
+        tmpImgGray = cv2.cvtColor(tmpImgGray,cv2.COLOR_BGR2GRAY)
+        equalityBool = (imgGray == tmpImgGray).all()
+        self.assertTrue(equalityBool)
 
 if __name__=="__main__":
     unittest.main()
