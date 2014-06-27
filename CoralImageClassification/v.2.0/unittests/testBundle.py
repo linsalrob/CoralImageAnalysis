b3841781f495cb167910be6264573126e154e89e
Salvatore Tosti <saltosti@gmail.com>
2014-06-27 13:46:22 -0700

    Updated testBundle.py
    Reduced repetition in testBundle.py, also added a list of files so it’s
    easy to update with later unit tests.

--- CoralImageClassification/v.2.0/unittests/testBundle.py
+++ CoralImageClassification/v.2.0/unittests/testBundle.py
@@ -2,10 +2,37 @@
 #A basic bundle script, which runs unittests for classes in Modules.
 
 import os
+def printRunning(process):
+    current = "Running "+process+ "..."
+    print current
+    
+def run(process):
+    printRunning(process)
+    os.system("python "+process)
 
+testList = [
+    "test_analysis_unittest.py",
+    "test_classification_unittest.py",
+    "test_contours_unittest.py",
+    "test_imageio_unittest.py",
+    "test_normalization_unittest.py"]
+for file in testList:
+    run(file)
+
+"""
 print "Starting unit tests..."
+printRunning("test_analysis_unittest.py")
 os.system("python test_analysis_unittest.py")
+
+printRunning("test_classification_unittest.py")
 os.system("python test_classification_unittest.py")
+
+printRunning("test_contours_unittest.py")
 os.system("python test_contours_unittest.py")
+
+printRunning("test_imageio_unittest.py")
 os.system("python test_imageio_unittest.py")
+
+printRunning("test_normalization_unittest.py")
 os.system("python test_normalization_unittest.py")
+"""
