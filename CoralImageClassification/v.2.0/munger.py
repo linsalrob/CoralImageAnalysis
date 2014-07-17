4f3e715fcaedd60031367b6d6faf64e8d22a0a77
Salvatore Tosti <saltosti@gmail.com>
2014-07-17 10:56:23 -0700

    Started work on munger.py
    Began work on munger.py, a compilation of functions for dealing with 2D
    ndarrays in python.

---
+++ CoralImageClassification/v.2.0/munger.py
@@ -0,0 +1,35 @@
+#munger.py
+#Data munging helper for 2D ndarrays from numpy package.
+
+def deleteFirstRow(array)):
+    """docstring for deleteFirstRow"""
+    pass
+
+def deleteFirstColumn(array):
+    """docstring for deleteFirstColumn"""
+    pass
+
+def deleteLastColumn(array):
+    """docstring for deleteLastColumn"""
+    pass
+
+def deleteLastRow(array):
+    """docstring for deleteLastRow"""
+    pass
+
+def deleteNthColumn(array):
+    """docstring for deleteNthColumn"""
+    pass
+    
+def deleteNthRow(array):
+    """docstring for deleteNthRow"""
+    pass
+
+def deleteRowSequence(array,start,end):
+    """docstring for deleteRowSequence"""
+    pass
+
+def deleteColumnSequence(array,start,end):
+    """docstring for deleteColumnSequence"""
+    pass
+
